72134d74e2db910368e7d6cef8707e85405fa61b
rgrannell1 <r.grannell2@gmail.com>
2019-08-02 23:19:48 +0100

    changed path

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,7 @@
 
 async function registerServiceWorker () {
   try {
-    const reg = await navigator.serviceWorker.register('./service-worker.js')
+    const reg = await navigator.serviceWorker.register('service-worker.js')
     console.log(`registered service-worker: scope is ${reg.scope}`)
   } catch (err) {
     console.error(`failed to register service-worker: ${err.message}`)
