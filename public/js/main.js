574dce4c4558e14f768e7211543b546ea97921a3
rgrannell1 <r.grannell2@gmail.com>
2019-08-13 21:13:28 +0100

    sw tweak

--- public/js/main.js
+++ public/js/main.js
@@ -3,7 +3,7 @@ import { request } from "https"
 
 async function registerServiceWorker () {
   try {
-    const reg = await navigator.serviceWorker.register('service-worker.js')
+    const reg = await navigator.serviceWorker.register('./../service-worker.js')
     console.log(`registered service-worker: scope is ${reg.scope}`)
   } catch (err) {
     console.error(`failed to register service-worker: ${err.message}`)
