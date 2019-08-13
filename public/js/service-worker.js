124839c8ae3c8a0960e6491634aa5eca627da41d
rgrannell1 <r.grannell2@gmail.com>
2019-08-13 20:32:48 +0100

    updated service worker

--- public/js/service-worker.js
+++ public/js/service-worker.js
@@ -1,12 +1,54 @@
 
+const cached = [
+  '/'
+]
+
+const service = {}
+
+service.install = async () => {
+  const cache = await caches.open('sw-cache')
+  await cache.addAll(cached)
+  return self.skipWaiting()
+}
+
+service.fetch = async () => {
+  const cachedRes = await caches.match(event.request)
+  if (cachedRes) {
+    return cachedRes
+  }
+
+  const uncachedRes = fetch(event.request)
+
+  const isUncacheable = !uncachedRes || uncachedRes.status !== 200 || uncachedRes.type !== 'basic'
+  if (isUncacheable) {
+    return uncachedRes;
+  }
+
+  const toCache = uncachedRes.clone()
+  const cache = await caches.open('sw-cache')
+  cache.put(event.request, toCache)
+
+  return uncachedRes
+}
+
 console.log(`⛏ service-worker started`)
 
 self.addEventListener('install', event => {
   console.log('⛏ installed')
+
+  event.waitUntil(service.install())
+})
+
+self.addEventListener('fetch', event => {
+  const isSameOrigin = event.request.url.startsWith(self.location.origin)
+
+  event.respondWith(service.fetch())
+
+  console.log('⛏ fetch')
 })
 
 self.addEventListener('activate', event => {
-  clients.claim()
+  // -- remove old caches, when needed
   console.log('⛏ activated')
 })
 
