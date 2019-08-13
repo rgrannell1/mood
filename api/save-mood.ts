dcd7f853d874de08168f5925a999016aa5823308
rgrannell1 <r.grannell2@gmail.com>
2019-08-13 22:34:19 +0100

    added mongo url

--- api/save-mood.ts
+++ api/save-mood.ts
@@ -1,5 +1,16 @@
 
-export default (req, res) => {
+const MongoClient = require('mongodb').MongoClient
+
+export default async (req, res) => {
+  const {
+    mongo_user,
+    mongo_password,
+    mongo_cluster
+  } = process.env
+
+  const url = `mongodb+srv://${mongo_user}:${mongo_password}@${mongo_cluster}.mongodb.net/test?retryWrites=true&w=majority`
+  const db = await MongoClient.connect(url)
+
   res.json({
     message: 'hi'
   })
