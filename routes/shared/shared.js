55ea4287e53aaa4abe86b1f07859931721825691
rgrannell1 <r.grannell2@gmail.com>
2019-08-28 23:09:55 +0100

    working on routing

--- routes/shared/shared.js
+++ routes/shared/shared.js
@@ -1,35 +1,39 @@
 
 import signale from 'signale'
-import errors from '@rgrannell/errors'
+//const errors = require('@rgrannell/errors')
+import * as errors from '@rgrannell/errors'
+//import * as createError from 'http-errors'
 
 const handleErrors = async (err, req, res) => {
-  console.log(err)
-
   res
-    .statusCode(500)
+    .status(500)
     .end('error handling not working ')
-
-  // -- add custom errors for handling.
 }
 
 export const routeMethod = methods => async (req, res) => {
-  signale.debug(`${req.method} ${req.path}`)
+  signale.debug(`${req.method} ${req.originalUrl}`)
 
   try {
     if (!methods.has(req.method)) {
-      const err = errors.METHOD_NOT_ALLOWED(`method ${req.method} not supported`, 'HTTP_405')
+      const err = errors.methodNotAllowed(`method ${req.method} not supported`, 405)
       throw err
     }
-    console.log(methods)
-    console.log(typeof methods)
 
-    await methods.get(req.method)(req, res)
+    const route = methods.get(req.method)
+
+    if (typeof route !== 'function') {
+      signale.error(`unsupported method ${req.method}`)
+      throw errors.internalServerError('server failed to handle request', 500)
+    }
+
+    await route(req, res)
 
   } catch (err) {
-    const isError = err instanceof Error
-    const requestError = isError
+    signale.warn(`error-response received: ${err.message ? err.message : err}`)
+
+    const requestError = err.message
       ? err
-      : errors.INTERNAL_SERVER_ERROR('server failed to handle request', 'HTTP_500')
+      : errors.internalServerError('server failed to handle request', 500)
 
     await handleErrors(requestError, req, res)
   }
