473034d7ac41863b766bff4634e3de042b14debe
rgrannell1 <r.grannell2@gmail.com>
2019-08-30 21:50:26 +0100

    refactored to spa

--- public/js/login.js
+++ public/js/login.js
@@ -1,7 +1,13 @@
 
-gapi.load('auth2', () => {
-  gapi.auth2.init()
-})
+window.onLoadCallback = function () {
+  if (!gapi) {
+    console.error('failed to load gapi')
+  }
+
+  gapi.load('auth2', () => {
+    gapi.auth2.init()
+  })
+}
 
 const onSignIn = googleUser => {
   const profile = googleUser.getBasicProfile()
