50e32ad303c918fc577dc0241e705b811b793c1e
rgrannell1 <r.grannell2@gmail.com>
2019-09-01 23:51:34 +0100

    added docs

--- public/js/services/local.js
+++ public/js/services/local.js
@@ -3,9 +3,22 @@
  * Typed wrapper for localstorage
  */
 const local = {
+/**
+ * set a value in localstorage
+ *
+ * @param {string} key the property name
+ * @param {string} value the property value
+ */
   set(key, value) {
     return localStorage.setItem(key, JSON.stringify(value))
   },
+/**
+ * set a value in localstorage
+ *
+ * @param {string} key the property name
+ *
+ * @returns {string} the value stored in localstorage
+ */
   get(key) {
     return JSON.parse(localStorage.getItem(key))
   }
