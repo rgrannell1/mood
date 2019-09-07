a563fd6a6819b68a6989d426f290b84145de8f01
rgrannell1 <r.grannell2@gmail.com>
2019-09-07 02:34:36 +0100

    removed build artifact

--- api/metadata.js
+++
@@ -1,8 +0,0 @@
-'use strict'
-exports.__esModule = true
-var routes_1 = require('./routes/shared/routes')
-var get_metadata_1 = require('./routes/get-metadata')
-var methods = new Map()
-methods.set('GET', get_metadata_1.default)
-var metadata = routes_1.routeMethod(methods)
-exports.default = metadata
