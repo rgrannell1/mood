3997780c0d9c1cdc89af65cabeb0b0510bba1628
rgrannell1 <r.grannell2@gmail.com>
2019-09-10 16:46:08 +0100

    tidied env var loading

--- routes/shared/config.js
+++ routes/shared/config.js
@@ -29,14 +29,18 @@ const parseGooglePrivateKey = content => {
   return JSON.parse(Buffer.from(content, 'base64'))
 }
 
+const readVariable = name => {
+  return process.env[name] || process.env[name.toLowerCase()]
+}
+
 module.exports = {
   test: {
-    credential: process.env.TEST_ACCOUNT_CREDENTIAL
+    credential: readVariable('TEST_ACCOUNT_CREDENTIAL')
   },
   google: {
     db: 'https://mood-251413.firebaseio.com',
-    clientId: process.env.google_client_id || process.env.GOOGLE_CLIENT_ID,
+    clientId: readVariable('GOOGLE_CLIENT_ID'),
     audience: '1053339394516-8m3pa0tvsejqha2usv84rkul7ja804s6.apps.googleusercontent.com',
-    privateKey: parseGooglePrivateKey(process.env.google_private_key || process.env.GOOGLE_PRIVATE_KEY)
+    privateKey: parseGooglePrivateKey(readVariable('GOOGLE_PRIVATE_KEY'))
   }
 }
