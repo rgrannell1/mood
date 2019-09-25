91ec6e9600a9f2463556e999ca4385254f22b71e
rgrannell1 <r.grannell2@gmail.com>
2019-09-25 04:16:32 +0100

    removed graph and added bg color

--- client/js/view/mood-graphs.js
+++ client/js/view/mood-graphs.js
@@ -31,51 +31,7 @@ const moodGraphs = {}
  * @param {Object}
  */
 moodGraphs.scatterplot = async data => {
-  const schema = {
-    $schema: 'https://vega.github.io/schema/vega-lite/v4.json',
-    description: 'Mood over time.',
-    mark: 'point',
-    width: 500,
-    height: 500,
-    encoding: {
-      x: {
-        field: 'timestamp',
-        type: 'temporal',
-        axis: {
-          grid: false,
-          title: 'date'
-        }
-      },
-      y: {
-        field: 'mood',
-        type: 'ordinal',
-        sort: moods
-      },
-      color: {
-        field: 'mood',
-        type: 'ordinal',
-        sort: moods,
-        scale: {
-          range: moods.map(pointColour)
-        }
-      }
-    },
-    data: {
-      values: data.moods
-    }
-  }
 
-  await vegaEmbed('#mood-over-time', schema)
-
-  /*
-  const view = new vega.View(vega.parse(schema), {
-    renderer: 'canvas',
-    container: '#mood-over-time',
-    hover: false
-  })
-
-  view.run()
-   */
 }
 
 export default moodGraphs
