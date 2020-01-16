756a46802fc85fb9f5f4636f2d20296c508bc583
rgrannell1 <r.grannell2@gmail.com>
2020-01-16 00:38:14 +0000

    added additional typings

--- server/mood.d.ts
+++ server/mood.d.ts
@@ -2,17 +2,32 @@
 export {}
 
 declare global {
-  type MoodRequest = RequestState & { state:RequestState } & ResponseMethods
+  type MoodRequest = RequestState & { state:RequestState } & {
+    body: any,
+    headers: { [key: string]: any },
+    method: 'GET' | 'POST' | 'PATCH' | 'DELETE'
+  }
+  type MoodError = Error & {
+    code?: string | number
+  }
   type MoodResponse = Response & ResponseMethods
   interface RequestState {
     trackingId?: string,
     ip?: string,
     forwardedFor?: string,
     userAgent?: string,
-    url?: string
+    url?: string,
+    userId?: string
   }
   interface ResponseMethods {
     status: any,
-    end: any
+    end: any,
+    setHeader: any,
+    writeHead: any
+  }
+
+  interface UserCredentials {
+    username: string,
+    password: string
   }
 }
